4a87ce49a3e0c96feedaedb035ae9fd152bf52bc
lucho <luchodd88@gmail.com>
2020-09-09 18:24:46 -0300

    initial commit Restaurante

---
+++ Restaurante/app/src/main/java/com/example/restaurante/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.restaurante;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void NavigateToCocina(View view)
+    {
+        Intent intent = new Intent(this, Cocina.class);
+        startActivity(intent);
+    }
+
+    public void NavigateToMozo(View view)
+    {
+        Intent intent = new Intent(this, Mozo.class);
+        startActivity(intent);
+    }
+}
