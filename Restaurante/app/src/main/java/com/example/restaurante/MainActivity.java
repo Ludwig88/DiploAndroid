18e6f3101f0de087f8a7aa90893e76f6d99e537d
lucho <luchodd88@gmail.com>
2020-10-01 09:34:23 -0300

    add logo on main

--- Restaurante/app/src/main/java/com/example/restaurante/MainActivity.java
+++ Restaurante/app/src/main/java/com/example/restaurante/MainActivity.java
@@ -2,21 +2,27 @@ package com.example.restaurante;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 
 public class MainActivity extends AppCompatActivity {
     public AlmacenStock m_almacenStock;
     public AlmacenPedidos m_almacenPedidos;
 
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         m_almacenPedidos = new AlmacenPedidos(this,1);
         m_almacenStock = new AlmacenStock(this,1);
+
+        ImageView logoImage = findViewById(R.id.imageView);
+        logoImage.setImageResource(R.raw.pngegg);
     }
 
     public void NavigateToCocina(View view)
